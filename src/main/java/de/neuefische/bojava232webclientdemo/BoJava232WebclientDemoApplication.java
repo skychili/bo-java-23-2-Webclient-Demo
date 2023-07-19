e168820ac8fedb9cd5d74e8af2a532f0858dac0f
Sara <sara.krieger2022@gmail.com>
2023-07-19 09:57:28 +0200

    Initial commit

---
+++ src/main/java/de/neuefische/bojava232webclientdemo/BoJava232WebclientDemoApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.bojava232webclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoJava232WebclientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoJava232WebclientDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/neuefische/bojava232webclientdemo/BoJava232WebclientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.bojava232webclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoJava232WebclientDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
